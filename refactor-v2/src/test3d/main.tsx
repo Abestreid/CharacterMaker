403b09e3f0487f960537937b070854628cd42604
Abeslab <121676892+Abestreid@users.noreply.github.com>
2026-08-21 22:26:59 +0300

    test3d: switch lab to CharacterMaker shape pack v3

--- refactor-v2/src/test3d/main.tsx
+++ refactor-v2/src/test3d/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Test3DAppV2 } from './Test3DAppV2';
+import { Test3DAppV3 } from './Test3DAppV3';
 
 const root = document.getElementById('body-test3d-root');
 
@@ -10,6 +10,6 @@ if (!root) {
 
 createRoot(root).render(
   <StrictMode>
-    <Test3DAppV2 />
+    <Test3DAppV3 />
   </StrictMode>,
 );
